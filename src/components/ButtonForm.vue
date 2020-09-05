6b815f7d984179fdeb69ca17f0d1d528a329b812
Tri Nguyen <tnguyen571@dxc.com>
2020-09-06 01:48:02 +0700

    deploy

--- src/components/ButtonForm.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <div class="form-group">
-    <button type="submit" class="btn btn-primary">{{ titleSubmit }}</button>
-    <router-link  class="btn btn-light" :to="{name: 'home'}">{{ $t('cancel') }}</router-link>
-  </div>
-</template>
-<script>
-export default { props: { titleSubmit: { type: String, required: true } } }
-</script>
